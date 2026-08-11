4df4fd58cf90d29bda57dd4d97d5a211763934ba
손창민 <ckdals4600@naver.com>
2026-08-11 16:19:55 +0900

    02: 가장 큰수

--- week02/online/PgsLargestNumber.java
+++ week02/online/PgsLargestNumber.java
@@ -1,5 +1,32 @@
 // 프로그래머스 42746 · 가장 큰 수
 // https://school.programmers.co.kr/learn/courses/30/lessons/42746
+import java.util.*;
+
 public class PgsLargestNumber {
+  
+
+  public String solution(int[] numbers) {
+    int n = numbers.length;
+    String[] nums = new String[n];
+    
+    for(int i=0; i<n; i++){
+      nums[i] = String.valueOf(numbers[i]);
+    }
+    
+    
+    Arrays.sort(nums, (s1,s2) -> {
+      return (s2+s1).compareTo(s1+s2);
+    });
+    
+    if(nums[0].equals("0")) return "0";
+    
+    StringBuilder answer = new StringBuilder();
+    
+    for(String s: nums){
+      answer.append(s);
+    }
+    
+    return answer.toString();
+  }
 
 }
